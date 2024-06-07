a4bdac111ea2bf5c1e5eeb41670c5a209c8736d9
billcoding <wangmiao07@sina.com>
2024-06-07 11:58:17 +0800

    feat: add callback & return supported

--- base.go
+++ base.go
@@ -45,3 +45,109 @@ func Pagination(db *gorm.DB, req PageReq, count *int64, list any) (err error) {
 
 	return
 }
+
+func Callback(err0 error, fns ...func()) (err error) {
+	if err = err0; err != nil {
+		return
+	}
+	for _, fn := range fns {
+		if fn != nil {
+			fn()
+		}
+	}
+	return
+}
+
+func Callback1[T any](err0 error, t T, fns ...func(t T)) (err error) {
+	if err = err0; err != nil {
+		return
+	}
+	for _, fn := range fns {
+		if fn != nil {
+			fn(t)
+		}
+	}
+	return
+}
+
+func Callback2[T1, T2 any](err0 error, t1 T1, t2 T2, fns ...func(t1 T1, t2 T2)) (err error) {
+	if err = err0; err != nil {
+		return
+	}
+	for _, fn := range fns {
+		if fn != nil {
+			fn(t1, t2)
+		}
+	}
+	return
+}
+
+func Callback3[T1, T2, T3 any](err0 error, t1 T1, t2 T2, t3 T3, fns ...func(t1 T1, t2 T2, t3 T3)) (err error) {
+	if err = err0; err != nil {
+		return
+	}
+	for _, fn := range fns {
+		if fn != nil {
+			fn(t1, t2, t3)
+		}
+	}
+	return
+}
+
+func CallbackErr(err0 error, fns ...func() (err error)) (err error) {
+	if err = err0; err != nil {
+		return
+	}
+	for _, fn := range fns {
+		if fn != nil {
+			if err = fn(); err != nil {
+				return
+			}
+		}
+	}
+	return
+}
+
+func Callback1Err[T any](err0 error, t T, fns ...func(t T) (err error)) (err error) {
+	if err = err0; err != nil {
+		return
+	}
+	for _, fn := range fns {
+		if fn != nil {
+			if err = fn(t); err != nil {
+				return
+			}
+		}
+	}
+	return
+}
+
+func Callback2Err[T1, T2 any](err0 error, t1 T1, t2 T2, fns ...func(t1 T1, t2 T2) (err error)) (err error) {
+	if err = err0; err != nil {
+		return
+	}
+	for _, fn := range fns {
+		if fn != nil {
+			if err = fn(t1, t2); err != nil {
+				return
+			}
+		}
+	}
+	return
+}
+
+func Callback3Err[T1, T2, T3 any](err0 error, t1 T1, t2 T2, t3 T3, fns ...func(t1 T1, t2 T2, t3 T3) (err error)) (err error) {
+	if err = err0; err != nil {
+		return
+	}
+	for _, fn := range fns {
+		if fn != nil {
+			if err = fn(t1, t2, t3); err != nil {
+				return
+			}
+		}
+	}
+	return
+}
+
+func Return[T any](t T, err0 error) (T, error) { return t, err0 }
