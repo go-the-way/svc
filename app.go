0b7da34a28b28f609dc6a9b68a281039d47cc990
billcoding <wangmiao07@sina.com>
2024-07-18 12:09:46 +0800

    feat: fix GetAppWithGroup

--- app.go
+++ app.go
@@ -17,8 +17,4 @@ var engine = gin.New()
 
 func GetApp(middlewares ...gin.HandlerFunc) *gin.Engine { engine.Use(middlewares...); return engine }
 
-func GetAppWithGroup(prefix string, middlewares ...gin.HandlerFunc) *gin.RouterGroup {
-	g := GetApp().Group(prefix)
-	g.Use(middlewares...)
-	return g
-}
+func GetAppWithGroup(prefix string) *gin.RouterGroup { return GetApp().Group(prefix) }
