29e29d8a8ed54cfda6a51929d82b4d158458bcf3
billcoding <wangmiao07@sina.com>
2025-05-09 17:32:40 +0800

    feat: add fs adapter for svc

--- adapter.go
+++ adapter.go
@@ -1,3 +1,14 @@
+// Copyright 2025 svc Author. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package svc
 
 import (
@@ -11,30 +22,43 @@ import (
 )
 
 type (
-	FSAdapter struct {
-		prefix string
-		fs     embed.FS
+	fsAdapter struct {
+		name, prefix string
+		dir          bool
+		fs           embed.FS
 	}
-	FileAdapter struct {
-		file fs.File
-	}
+	fileAdapter struct{ file fs.File }
 )
 
-func NewFSAdapter(prefix string, fs embed.FS) http.FileSystem {
-	return &FSAdapter{
-		prefix: strings.Trim(prefix, "/"),
-		fs:     fs,
-	}
+// The fs adapter usage:
+//
+// //go:embed testdata.json
+// var fileFS embed.FS
+//
+// //go:embed scripts/
+// var dirFS embed.FS
+//
+// func init() {
+// 	svc.GetApp().StaticFileFS("/testdata.json", "testdata.json", svc.FSFileAdapter("testdata.json", fileFS))
+// 	svc.GetApp().StaticFS("/scripts", svc.FSDirAdapter("scripts", dirFS))
+// }
+
+func FSFileAdapter(name string, fs embed.FS) http.FileSystem {
+	return &fsAdapter{name: name, fs: fs}
 }
 
-func NewFileAdapter(file fs.File) http.File {
-	return &FileAdapter{file: file}
+func FSDirAdapter(prefix string, fs embed.FS) http.FileSystem {
+	return &fsAdapter{prefix: strings.Trim(prefix, "/"), dir: true, fs: fs}
 }
 
-func (a *FSAdapter) Open(name string) (http.File, error) {
+func newFileAdapter(file fs.File) http.File { return &fileAdapter{file} }
+
+func (a *fsAdapter) Open(name string) (http.File, error) {
 	name = strings.TrimPrefix(name, "/")
-	if a.prefix != "" {
-		name = path.Join(a.prefix, name)
+	if a.dir {
+		if a.prefix != "" {
+			name = path.Join(strings.Trim(a.prefix, "/"), name)
+		}
 	}
 	f, err := a.fs.Open(name)
 	if err != nil {
@@ -54,10 +78,10 @@ func (a *FSAdapter) Open(name string) (http.File, error) {
 		file.Close()
 		return nil, errors.New("is a directory")
 	}
-	return NewFileAdapter(file), nil
+	return newFileAdapter(file), nil
 }
 
-func (f *FileAdapter) Seek(offset int64, whence int) (int64, error) {
+func (f *fileAdapter) Seek(offset int64, whence int) (int64, error) {
 	seeker, ok := f.file.(io.Seeker)
 	if !ok {
 		return 0, fs.ErrInvalid
@@ -65,7 +89,7 @@ func (f *FileAdapter) Seek(offset int64, whence int) (int64, error) {
 	return seeker.Seek(offset, whence)
 }
 
-func (f *FileAdapter) Readdir(count int) ([]fs.FileInfo, error) {
+func (f *fileAdapter) Readdir(count int) ([]fs.FileInfo, error) {
 	dir, ok := f.file.(fs.ReadDirFile)
 	if !ok {
 		return nil, fs.ErrInvalid
@@ -85,7 +109,7 @@ func (f *FileAdapter) Readdir(count int) ([]fs.FileInfo, error) {
 	return fileInfos, nil
 }
 
-func (f *FileAdapter) Read(p []byte) (int, error) {
+func (f *fileAdapter) Read(p []byte) (int, error) {
 	reader, ok := f.file.(io.Reader)
 	if !ok {
 		return 0, fs.ErrInvalid
@@ -93,6 +117,6 @@ func (f *FileAdapter) Read(p []byte) (int, error) {
 	return reader.Read(p)
 }
 
-func (f *FileAdapter) Close() error { return f.file.Close() }
+func (f *fileAdapter) Close() error { return f.file.Close() }
 
-func (f *FileAdapter) Stat() (fs.FileInfo, error) { return f.file.Stat() }
+func (f *fileAdapter) Stat() (fs.FileInfo, error) { return f.file.Stat() }
